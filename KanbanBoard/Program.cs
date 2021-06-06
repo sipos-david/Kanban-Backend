b3bffac2f80e33ffc61c262c238ea2787037b83d
Sipos Aron David <sipisipos@gmail.com>
2021-06-06 18:09:33 +0200

    Added CosmosDb to backend

--- KanbanBoard/Program.cs
+++ KanbanBoard/Program.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace KanbanBoard
 {
@@ -41,6 +42,13 @@ namespace KanbanBoard
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(
+keyVaultEndpoint,
+new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
